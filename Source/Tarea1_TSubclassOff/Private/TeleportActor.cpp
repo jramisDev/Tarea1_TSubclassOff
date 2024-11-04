ce7aef152420e736d6c44c3da984d67c5389d855
Jesús Ramis Galindo <113546651+jramisDev@users.noreply.github.com>
2024-11-04 20:36:43 +0100

    JRG - TP

---
+++ Source/Tarea1_TSubclassOff/Private/TeleportActor.cpp
@@ -0,0 +1,67 @@
+﻿#include "TeleportActor.h"
+
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+#include "Tarea1_TSubclassOff/Tarea1_TSubclassOffCharacter.h"
+
+
+ATeleportActor::ATeleportActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	TeleportMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Teleport Mesh"));	
+	// TeleportMesh->SetWorldRotation(FRotator{90.f,0.f,0.f});
+	RootComponent = TeleportMesh;
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
+	BoxCollision->SetupAttachment(TeleportMesh);
+	
+	BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ATeleportActor::BoxCollisionBeginOverlap);	
+	BoxCollision->OnComponentEndOverlap.AddDynamic(this, &ATeleportActor::BoxCollisionEndOverlap);
+	
+	ArrowDirection = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow Direction"));
+	ArrowDirection->SetupAttachment(TeleportMesh);
+}
+
+void ATeleportActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATeleportActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATeleportActor::BoxCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(bTeleportingActor) return;
+	
+	UE_LOG(LogTemp, Display, TEXT("ORIGEN TEST1"));
+	if(ATarea1_TSubclassOffCharacter* Character = Cast<ATarea1_TSubclassOffCharacter>(OtherActor))
+	{
+		UE_LOG(LogTemp, Display, TEXT("ORIGEN TEST2"));
+		if(TeleportDestination)
+		{
+			UE_LOG(LogTemp, Display, TEXT("ORIGEN TEST3"));
+			if(!bTeleportingActor)
+			{
+				bTeleportingActor = true;
+				Character->SetActorLocationAndRotation(TeleportDestination->GetActorLocation(), TeleportDestination->GetActorRotation());
+			}
+		}
+	}
+}
+
+void ATeleportActor::BoxCollisionEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Display, TEXT("DESTINO TEST1"));
+	if(ATarea1_TSubclassOffCharacter* Character = Cast<ATarea1_TSubclassOffCharacter>(OtherActor))
+	{
+		UE_LOG(LogTemp, Display, TEXT("DESTINO TEST2"));
+		bTeleportingActor = false;
+	}
+}
---
+++ Source/Tarea1_TSubclassOff/Public/TeleportActor.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TeleportActor.generated.h"
+
+class UArrowComponent;
+class UBoxComponent;
+
+UCLASS()
+class TAREA1_TSUBCLASSOFF_API ATeleportActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ATeleportActor();
+
+	UPROPERTY(EditInstanceOnly, Category="Teleport")
+	ATeleportActor* TeleportDestination;
+	
+	UFUNCTION(BlueprintCallable, Category="Teleport")
+	void BoxCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+	                              const FHitResult& SweepResult);
+	
+	UFUNCTION(BlueprintCallable, Category="Teleport")
+	void BoxCollisionEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Teleport")
+	bool bTeleportingActor;
+	
+protected:
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Teleport")
+	UStaticMeshComponent* TeleportMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Teleport")
+	UBoxComponent* BoxCollision;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Teleport")
+	UArrowComponent* ArrowDirection;
+	
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
